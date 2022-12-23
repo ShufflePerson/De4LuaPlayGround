734dfb73518d9d1757f626f0f5f1406bd49c04a5
Shuffle21 <>
2022-12-23 23:59:33 +0300

    Initial release

---
+++ src/server.js
@@ -0,0 +1,62 @@
+
+//express server which servers public folder
+
+const express = require('express');
+const child_process = require('child_process');
+const fs = require('fs');
+
+const app = express();
+
+const port = 80;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }))
+
+
+function random_str(len) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < len; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+async function deobfucasate(code, res) {
+    let id = random_str(10)
+    let input = "scripts/" + id + "_input.lua"
+    let output = "scripts/" + id + "_output.lua"
+
+    fs.writeFileSync(input, code)
+
+    try {
+        child_process.execSync(`cd De4Lua && npm run start ../${input} ../${output}`)
+        let deobfuscatedCode = fs.readFileSync(output, "utf-8")
+        //replace new lines with <br> so it can be displayed in html
+        deobfuscatedCode = deobfuscatedCode.replace(/\n/g, "<br>")
+        //display "    " correctly
+        deobfuscatedCode = deobfuscatedCode.replace(/    /g, "&nbsp;&nbsp;&nbsp;&nbsp;")
+        res.send(deobfuscatedCode)
+
+    } catch (error) {
+        res.send("Error")
+        return
+    }
+}
+
+
+app.post('/deobfuscate', (req, res) => {
+    const { code } = req.body;
+    if (!code) {
+        return res.send("Error")
+    }
+
+    deobfucasate(code, res)
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
